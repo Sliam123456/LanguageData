4bc5a200f7d7c5809269b7ea9a5e1d89a89d370a
Sliam <www.liamthomas.com@gmail.com>
2025-09-10 14:32:30 +0200

    Update json format

--- index.mjs
+++ index.mjs
@@ -2,14 +2,19 @@
 import fs from "fs";
 
 const query = fs.readFileSync("TopLanguagesQuery.gql", "utf-8");
-const response = await fetch('https://api.github.com/graphql', {
+const getResponse = async (cursor = null) => (await fetch('https://api.github.com/graphql', {
     method: 'POST',
-    body: `{ "query": ${JSON.stringify(query)}`,
+    body: `{ "query": ${JSON.stringify(query)}, "variables": { "cursor": ${JSON.stringify(cursor)}}`,
     headers: { 'Authorization': `Bearer ${process.argv[2]}` }
-}).then(async res => await res.json());
+}).then(async res => await res.json())).data.user.repositories;
+let response = await getResponse();
+let repos = response.nodes;
+while (response.pageInfo.hasNextPage) {
+    response = await getResponse(response.pageInfo.endCursor);
+    repos.push(...response.nodes);
+}
 let languages = new Map();
-
-for (const node of response.data.user.repositories.nodes) {
+for (const node of repos) {
     for (const edge of node.languages.edges) {
         languages.set(edge.node.name, {
             size: edge.size + (languages.get(edge.node.name) ? languages.get(edge.node.name).size : 0),
@@ -17,4 +22,4 @@ for (const node of response.data.user.repositories.nodes) {
         });
     }
 }
-fs.writeFileSync("LanguageData.json", JSON.stringify(new Map([...languages].sort((a, b) => b[1].size - a[1].size)), (_key, value) => value instanceof Map ? [...value] : value, 4));
+fs.writeFileSync("LanguageData.json", JSON.stringify(Object.fromEntries([...languages].sort((a, b) => b[1].size - a[1].size)), null, 4));
